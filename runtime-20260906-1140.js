30b043177dd03dfec884c92163947a5bbc34400e
ProtChan <prot.pompom@gmail.com>
2026-09-11 01:17:28 +0900

    Refresh runtime after derived TRYJPY fix

--- runtime-20260906-1140.js
+++ runtime-20260906-1140.js
@@ -1,5 +1,5 @@
 (() => {
-  const BUILD = '20260911-0100';
+  const BUILD = '20260911-0115';
   const showError = (message) => {
     document.documentElement.dataset.appLoadError = message;
     const bar = document.createElement('div');
@@ -50,7 +50,7 @@
       (0, eval)(`${defaultUnitsNormalizedAppSource}\n${accountingSource}\n${swapDecimalSource}\n${calendarSource}\n${pwaSource}\n${accessLayoutSource}\n${swapPrecisionSource}\n${hiroseHistorySource}\n${hiroseRateHistorySource}\n${hirosePendingBootstrapSource}\n${hiroseInputSettleSource}\n${closeConversionSource}\n${capitalHistorySource}\n${rateEditPerformanceSource}\n${userPreparedRateSource}\n${worstAskRiskSource}\n${referenceDataRebuildSource}\n${privatePublisherDailyLayoutSource}\n${liveRateRefreshSource}\n${sameDaySwapValuationSource}\n${sameDaySwapReinforceSource}\n${valuationSaveFinalSource}\n//# sourceURL=dollar-to-lira-${BUILD}.js`);
       document.documentElement.dataset.runtimeBuild = BUILD;
       const status = document.querySelector('.local-status');
-      if (status) status.innerHTML = '<i></i>LOCAL · 0100';
+      if (status) status.innerHTML = '<i></i>LOCAL · 0115';
     })
     .catch((error) => showError(error?.message || String(error)));
 })();
